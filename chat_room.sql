a565d8f284e381812632a56dc932dbb68067b14c
ichiYuuki <mrofushi@gmail.com>
2016-01-09 17:59:59 +0900

    修正バージョンSQL

--- chat_room.sql
+++ chat_room.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2016 年 1 朁E09 日 08:48
+-- Generation Time: 2016 年 1 朁E09 日 09:58
 -- サーバのバージョン： 5.6.25
 -- PHP Version: 5.6.11
 
@@ -23,16 +23,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- テーブルの構造 `comment`
+-- テーブルの構造 `comments`
 --
 
-CREATE TABLE IF NOT EXISTS `comment` (
+CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL,
   `modified` datetime NOT NULL,
   `created` datetime NOT NULL,
   `text` varchar(128) NOT NULL,
-  `user_name` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `user_name` varchar(32) NOT NULL,
+  `title_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `comments`
+--
+
+INSERT INTO `comments` (`id`, `modified`, `created`, `text`, `user_name`, `title_id`) VALUES
+(2, '2016-01-09 09:42:05', '2016-01-09 09:42:05', 'ヤッホー', 'いいちかわ', 2);
 
 -- --------------------------------------------------------
 
@@ -44,18 +52,24 @@ CREATE TABLE IF NOT EXISTS `titles` (
   `id` int(11) NOT NULL,
   `modified` datetime NOT NULL,
   `created` datetime NOT NULL,
-  `titel_name` varchar(64) NOT NULL,
-  `coment_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `titel_name` varchar(64) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `titles`
+--
+
+INSERT INTO `titles` (`id`, `modified`, `created`, `titel_name`) VALUES
+(2, '2016-01-09 09:41:39', '2016-01-09 09:41:39', '教室');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `comment`
+-- Indexes for table `comments`
 --
-ALTER TABLE `comment`
+ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -69,15 +83,15 @@ ALTER TABLE `titles`
 --
 
 --
--- AUTO_INCREMENT for table `comment`
+-- AUTO_INCREMENT for table `comments`
 --
-ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `titles`
 --
 ALTER TABLE `titles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
